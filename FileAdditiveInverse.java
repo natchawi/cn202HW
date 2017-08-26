1dea0c27dc4470139530477a258a843a9f8824c0
natchawi <natchawi_@hotmail.com>
2017-08-27 01:00:41 +0700

    Update FileAdditiveInverse.java

--- FileAdditiveInverse.java
+++ FileAdditiveInverse.java
@@ -1,9 +1,4 @@
-
 import java.io.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.StringTokenizer;
 
 /**
@@ -12,30 +7,31 @@ import java.util.StringTokenizer;
  */
 public class FileAdditiveInverse {
 
-    /**
-     * @param args the command line arguments
-     */
+    
   public static void main(String[] args) throws IOException {
-    try{
-	FileReader reader = new FileReader(args[0]);
-        BufferedReader input = new BufferedReader(reader);
+     FileReader reader = null;
+     BufferedReader input = null;
+     PrintWriter out = null;
+      try{
+	reader = new FileReader(args[0]);
+        input = new BufferedReader(reader);
 	FileWriter writer = new FileWriter(args[1]);
-	//PrintWriter out = new PrintWriter(writer);
+	out = new PrintWriter(writer);
         int inverse = 0 ;
         while(true){
             String str = input.readLine();
             if(str == null)break;
-            StringTokenizertokenizer = new StringTokenizer(str);
+            StringTokenizer tokenizer= new StringTokenizer(str);
             while(tokenizer.hasMoreTokens()){
                 String token = tokenizer.nextToken();
                 inverse = -1 * Integer.parseInt(token);
-		writer.print(inverse + " ");
+		out.print(inverse + " ");
             }
-		writer.println();
+		out.println();
         }
      }finally{
 	if(input != null)input.close();
-	if(writer != null)writer.close();
+	if(out != null)out.close();
 	}
         
    }
