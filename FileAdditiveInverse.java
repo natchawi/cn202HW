6beb6caa58e11c03ccc59a7730a0f8679536c906
centos <natchawi@localhost.localdomain>
2017-08-27 06:54:27 +0700

    First Commit

---
+++ FileAdditiveInverse.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Yooyii
+ */
+public class FileAdditiveInverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader(args[0]);
+        BufferedReader input = new BufferedReader(reader);
+        while(true){
+            String str = input.readLine();
+            if(str == null)break;
+            StringTokenizer
+        }
+        
+    }
+    
+}
+
